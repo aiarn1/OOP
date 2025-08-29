939806a67f9d4b8d9331f0ac98142d3a995c9ef5
Papellero <ACT-STUDENT@LAB3-PC28>
2025-08-29 14:17:25 +0800

    Hii

---
+++ Cuyos_EncapsulationUserInput/Cuyos_EncapsulationUserInput/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_EncapsulationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            // Louis Aian Cuyos
+            // IT306
+            // EXER_05: Applying Encapsulation with User
+
+
+            // Instantiate of an Object Class
+            Shirt myshirt = new Shirt();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\t\tEnter the Brand and Color of the Shirt that you want");
+
+            // User Input
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\n\nEnter the Brand of your Shirt : ");
+            myshirt.Name = Console.ReadLine();
+
+            Console.Write("Enter the Color of you Shirt  : ");
+            myshirt.Color = Console.ReadLine();
+
+            // Call Method to Display
+            myshirt.DisplayDetails();
+
+            Console.ReadKey();
+
+            // If you want to input again
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nEnter if you want more (Y/N)");
+            char i = Convert.ToChar(Console.ReadLine());
+
+            if (i == 'Y')
+            {
+                goto Main;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Cuyos_EncapsulationUserInput/Cuyos_EncapsulationUserInput/Shirt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_EncapsulationUserInput
+{
+    internal class Shirt
+    {
+        // Encap Properties for Fields
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+
+
+        // Method to Display shirt details
+        public void DisplayDetails()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan; // Color
+
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\t******** Here are the Details of the Shoes you inputed *********");
+            Console.WriteLine("\nEnter the Brand of your shoes: " + Name);
+            Console.WriteLine("Enter the Color of your shoes: " + Color);
+
+        }
+    }
+}
