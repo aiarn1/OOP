7e32bbaff0e58bdda4282f5beab897287330f5b1
Papellero <ACT-STUDENT@LAB3-PC28>
2025-08-29 14:11:41 +0800

    Hii

---
+++ Cuyos_PropertiesAndEncapsulation/Cuyos_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Louis Aian Cuyos
+            // IT306
+            // EXER_05: Applying Encapsulation with User
+
+
+            // Instantiate of an Object Class
+            Shirt myshirt = new Shirt();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+             
+
+            myshirt.Name = "Nike";  // Set Name Property
+            myshirt.Color = "Black"; // Set Color Property
+
+            // Call Method to Display
+            myshirt.DisplayDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cuyos_PropertiesAndEncapsulation/Cuyos_PropertiesAndEncapsulation/Shirt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_PropertiesAndEncapsulation
+{
+    internal class Shirt
+    {
+        // Encap Properties for Fields
+        private string name;
+        private string color;
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+
+
+        // Method to Display shirt details
+        public void DisplayDetails()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan; // Color
+
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+     
+            Console.WriteLine("\nName : " + Name);
+            Console.WriteLine("Shoes : " + Color);
+
+        }
+    }
+}
